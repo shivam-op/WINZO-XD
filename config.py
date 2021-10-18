1dd7bd8794a9ff6a2da2c8fe5b9ffcc1f96e060a
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-10-18 14:44:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ load_dotenv()
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Camila Music")
+BOT_USERNAME = getenv("BOT_USERNAME")
+PLAYER_USERNAME = getenv("PLAYER_USERNAME")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
